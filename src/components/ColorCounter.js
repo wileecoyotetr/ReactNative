2d20b69fec90c7aa217be0256bfabc8d5e8fccbf
Enes <enes@bilisim.com.tr>
2022-11-02 17:49:33 +0300

    2022 11 02 iş commit

--- src/components/ColorCounter.js
+++ src/components/ColorCounter.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { View, Text, StyleSheet, Image, Button } from "react-native";
 
-const ColorCounter = ({color}) => {
+const ColorCounter = ({color, onIncrease, onDecrease}) => {
     return <View>
         <Text>{color}</Text>
-        <Button title={`Increase ${color}`}/>
-        <Button title={`Decrease  ${color}`}/>
+        <Button onPress={() => onIncrease()} title={`Increase ${color}`}/>
+        <Button onPress={() => onDecrease()}  title={`Decrease  ${color}`}/>
     </View>
 };
 
